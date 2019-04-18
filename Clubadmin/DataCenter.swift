067fbbe5bc3a89118eda082fad9c35cc2dd4b816
cadenzah <cadenzah93@gmail.com>
2019-04-18 18:06:48 +0900

    Worked with TaskTableViewController.swift

--- Clubadmin/DataCenter.swift
+++ Clubadmin/DataCenter.swift
@@ -27,17 +27,27 @@ struct Task {
     }
     
     mutating func editDesc (by newDesc: String) -> Void {
+        var resultDesc: String = ""
+        if let prevDesc = self.taskDesc {
+            resultDesc += " \(prevDesc)"
+        }
         
+        resultDesc += newDesc
+        self.taskDesc = resultDesc
+    }
+    
+    mutating func addOwner (_ newOwner: String) -> Void {
+        self.taskOwner += [newOwner]
     }
 }
 
-var task1:Task = Task(name: "밥 먹기", owner: "김정모")
-var task2:Task = Task(name: "라즈베리파이3 세팅", owner: "김정모")
-var task3:Task = Task(name: "운동하기", owner: "김정모")
-var task4:Task = Task(name: "제본 맡긴 책 찾으러 가기", owner: "김정모")
-var task5:Task = Task(owner: "김정모")
-var task6:Task = Task(name: "동아리 회의 참석", owner: "김정모")
-var task7:Task = Task(name: "스터디 준비", owner: "김정모")
-var task8:Task = Task(name: "여자친구 만나기", owner: "김정모")
-var task9:Task = Task(name: "일일퀘스트 완료하기", owner: "김정모")
-var task10:Task = Task(name: "Swift 문법 공부", owner: "김정모")
+var tasks:[Task] = [Task(name: "밥 먹기", owner: "김정모"),
+                    Task(name: "라즈베리파이3 세팅", owner: "김정모"),
+                    Task(name: "운동하기", owner: "김정모"),
+                    Task(name: "제본 맡긴 책 찾으러 가기", owner: "김정모"),
+                    Task(owner: "김정모"),
+                    Task(name: "동아리 회의 참석", owner: "김정모"),
+                    Task(name: "스터디 준비", owner: "김정모"),
+                    Task(name: "여자친구 만나기", owner: "김정모"),
+                    Task(name: "일일퀘스트 완료하기", owner: "김정모"),
+                    Task(name: "Swift 문법 공부", owner: "김정모")]
